86673382e4814904f9cf12d04668dcd94e203403
Vladimir <bvanja.87@gmail.com>
2017-06-27 18:58:44 +0200

    alert changed

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 // Enemy class
 var Enemy = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.sprite = 'images/enemy-bug.png';
-  this.multiplier = Math.floor((Math.random() * 4) + 1);
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/enemy-bug.png';
+    this.multiplier = Math.floor((Math.random() * 15) + 10);
 };
 
 // Enemy methods
 Enemy.prototype = {
-  update: function(dt) {
-    this.x = this.x + 75 * dt * this.multiplier;
-    // Reset the enemy position
-    if(this.x > 700) {
-      this.x = - 150;
+    update: function(dt) {
+        this.x = this.x + 40 * dt * this.multiplier;
+        // Reset the enemy position
+        if (this.x > 700) {
+            this.x = -150;
+        }
+        // Collision control
+        if (this.x > player.x - 50 && this.x < player.x + 50 && this.y > player.y - 50 && this.y < player.y + 50) {
+            player.x = player.xStart;
+            player.y = player.yStart;
+        }
+    },
+    // Draw the enemy on the screen
+    render: function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    // Collision control
-    if(this.x > player.x - 50 && this.x < player.x + 50 && this.y > player.y - 50 && this.y < player.y + 50) {
-      player.x = player.xStart;
-      player.y = player.yStart;
-    }
-  },
-  // Draw the enemy on the screen
-  render: function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
 };
 
 
@@ -33,67 +33,69 @@ Enemy.prototype = {
 
 // Player class
 var Player = function(x, y) {
-  this.x = x;
-  this.y = y;
-  // Set reset position
-  this.xStart = x;
-  this.yStart = y;
+    this.x = x;
+    this.y = y;
+    // Set reset position
+    this.xStart = x;
+    this.yStart = y;
 
-  // Draw the poor fella
-  this.sprite = 'images/char-boy.png';
+    // Draw the poor fella
+    this.sprite = 'images/char-boy.png';
 };
 
 // Player methods
 Player.prototype = {
-  update: function() {
-    this.x = this.x;
-    this.y = this.y;
-  },
+    update: function() {
+        this.x = this.x;
+        this.y = this.y;
+    },
 
-  render: function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  },
+    // Draw the player on the screen
+    render: function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    },
 
-  handleInput: function(controls) {
-    // Movement controls
-    switch (controls) {
-      case 'up':
-        var move = this.y -= 85;
-        break;
-      case 'down':
-        var move = this.y += 85;
-        break;
-      case 'right':
-        var move = this.x += 100;
-        break;
-      case 'left':
-        var move = this.x -= 100;
+    handleInput: function(controls) {
+        // Movement controls
+        switch (controls) {
+            case 'up':
+                this.y -= 85;
+                break;
+            case 'down':
+                this.y += 85;
+                break;
+            case 'right':
+                this.x += 100;
+                break;
+            case 'left':
+                this.x -= 100;
+        }
+        // Confine players movement
+        if (this.x === 500) {
+            this.x = 400;
+        } else if (this.x < 0) {
+            this.x = this.xStart;
+        } else if (this.y > 400) {
+            this.y = 390;
+        } else if (this.y < 0) {
+            alert("Well done! You showed those bugs who's the boss!");
+            this.x = this.xStart;
+            this.y = this.yStart;
+        }
     }
-    // Confine players movement
-    if(this.x === 500) {
-      this.x = 400;
-    } else if (this.x < 0) {
-      this.x = this.xStart;
-    } else if (this.y > 400) {
-      this.y = 390;
-    } else if (this.y < 0) {
-      alert('Well done!');
-      this.y = this.yStart;
-    }
-  }
 };
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
 
 
